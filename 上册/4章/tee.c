aa6b7ee65c5dce04e2b6eadd02807c4b09a417be
xyz2781790037 <2781790037@qq.com>
2024-11-26 22:46:51 +0800

    file

--- 上册/4章/tee.c
+++ 上册/4章/tee.c
@@ -1,83 +1,61 @@
-// #include<sys/stat.h>
-// #include<fcntl.h>
-// #include<unistd.h>
-// #include<stdio.h>
-// #include<stdlib.h>
-// void tee(const char* dest)
-// {
-//     int destination;
-//     char buffer[1024];
-//     fgets(buffer, 1024, stdin);
-//     destination = open(dest, O_WRONLY | O_CREAT | O_APPEND,0644);//0644文件所有者具有读写权利，其他只读
-//     size_t n;
-//     write(destination, buffer, 1024);
-
-//     close(destination);
-//     printf("File write to %s\n", dest);
-// }
-// int main(int argc,char*argv[])
-// {
-//     if(argc != 2)
-//     {
-//         printf("%s is error.\n", argv[0]);
-//         exit(1);
-//     }
-//     tee(argv[1]);
-//     return 0;
-// }
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void tee(const char *dest)
+void tee(const char *dest, int argc)
 {
     int destination;
     char buffer[1024];
-    ssize_t bytes_read;
-
-    // 读取用户输入，避免多余的空白字符
-    bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer) - 1); // 读入标准输入
-
-    if (bytes_read < 0)
+    ssize_t n;
+    n = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+    if (n < 0)
     {
         perror("Failed to read input");
         exit(1);
     }
-
-    // 确保读取到的字符串以 '\0' 结束
-    buffer[bytes_read] = '\0';
-
-    // 打开文件进行写操作（如果文件不存在，则创建并写入；如果存在，则追加内容）
-    destination = open(dest, O_WRONLY | O_CREAT | O_APPEND, 0644); // 0644: 用户读写权限，其他用户只读
-
+    if(n > 0 && buffer[n - 1] == '\n')
+    {
+        n--;
+    }
+    buffer[n] = '\0';
+    if (argc == 2)
+    {
+        destination = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0644); // 0644: 用户读写权限，其他用户只读
+    }
+    else
+    {
+        destination = open(dest, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    }
     if (destination < 0)
     {
         perror("Failed to open file");
         exit(1);
     }
-
-    // 写入读取到的内容
-    if (write(destination, buffer, bytes_read) < 0)
+    if (write(destination, buffer, n) < 0)
     {
         perror("Failed to write to file");
         close(destination);
         exit(1);
     }
-
     close(destination);
     printf("Data written to %s\n", dest);
 }
-
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc != 2 && argc != 3)
     {
-        printf("Usage: %s <filename>\n", argv[0]);
+        printf("%s is error\n", argv[0]);
         exit(1);
     }
-
-    tee(argv[1]);
+    if (argc == 2)
+    {
+        tee(argv[1], argc);
+    }
+    else if (argc == 3)
+    {
+        tee(argv[2], argc);
+    }
     return 0;
 }
---
+++ 上册/5/5-3.c
@@ -0,0 +1,52 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void tee(const char *dest, int num_byte, int argc)
+{
+    int destination;
+    char buffer = 'a';
+    ssize_t n;
+    if (argc == 3)
+    {
+        destination = open(dest, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+    }
+    else if (argc == 4)
+    {
+        destination = open(dest, O_WRONLY | O_CREAT, 0644);
+        
+    }
+    if (destination < 0)
+    {
+        perror("Failed to open file");
+        exit(1);
+    }
+    for (int i = 0; i < num_byte;i++)
+    {
+        if(argc == 4)
+        {
+            if(lseek(destination, 0, SEEK_END) < 0)
+            {
+                perror("Fail to seek the file");
+            }
+        }
+        if (write(destination, &buffer, 1) < 0)
+        {
+            perror("Failed to write to file");
+            close(destination);
+            exit(1);
+        }
+    }
+        
+    close(destination);
+    printf("Data written to %s\n", dest);
+}
+int main(int argc, char *argv[])
+{
+    int num;
+    sscanf(argv[2], "%d", &num);
+    tee(argv[1], num, argc);
+    return 0;
+}
