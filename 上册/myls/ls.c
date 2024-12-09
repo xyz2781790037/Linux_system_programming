71ed62c7a36d10c7e02b5aebdacbc77a9ee710d3
xyz2781790037 <2781790037@qq.com>
2024-12-09 22:45:17 +0800

    newfile

--- 上册/myls/ls.c
+++ 上册/myls/ls.c
@@ -16,21 +16,7 @@
 #include <pwd.h>
 #include <grp.h>
 #include <time.h>
-int judge_file_or_directory(const char *c)
-{
-    struct stat st;
-    if (stat(c, &st) == -1)
-    {
-        perror("文件或目录不存在或无法访问");
-        exit(1);
-    }
-    if (S_ISREG(st.st_mode))
-    {
-        return 1;
-    }
-    return 0;
-}
-int filesort_a(const struct dirent **a, const struct dirent **b)
+int filesort(const struct dirent **a, const struct dirent **b)
 {
     char aw[256], bw[256];
     if ((*a)->d_name[0] == '.' && strcmp((*a)->d_name, ".") != 0 && strcmp((*a)->d_name, "..") != 0)
@@ -55,197 +41,177 @@ int filesort_a(const struct dirent **a, const struct dirent **b)
     }
     return strcasecmp(aw, bw);
 }
-int filesort(const struct dirent **a, const struct dirent **b)
-{
-    return strcasecmp((*a)->d_name, (*b)->d_name);
-}
-void mylsbase(struct dirent **fina, const char *c)
-{
-    int n;
-    if (judge_file_or_directory(c))
-    {
-        printf("%s", c);
-    }
-    else
-    {
-        n = scandir(c, &fina, NULL, filesort);
-        if (n == -1)
-        {
-            perror("scandir");
-            exit(EXIT_FAILURE);
-        }
-        for (int i = 0; i < n; i++)
-        {
-            if (strcmp(fina[i]->d_name, "..") != 0 && strncmp(fina[i]->d_name, ".", 1))
-            {
-                printf("%s\n", fina[i]->d_name);
-            }
-            free(fina[i]);
-        }
-        free(fina);
-    }
-}
-void myls_a(struct dirent **fina, const char *c)
-{
-    int n;
-    if (judge_file_or_directory(c))
-    {
-        printf("%s", c);
-    }
-    else
-    {
-        n = scandir(c, &fina, NULL, filesort_a);
-        if (n == -1)
-        {
-            perror("scandir");
-            exit(EXIT_FAILURE);
-        }
-        for (int i = 0; i < n; i++)
-        {
-            printf("%s\n", fina[i]->d_name);
-            free(fina[i]);
-        }
-        free(fina);
-    }
-}
-void myls_s(struct dirent **file) {}
-void myls_l(struct dirent **file, const char *c)
+int judge_file_or_directory(const char *c)
 {
     struct stat st;
-    int n;
-    n = scandir(c, &file, NULL, filesort);
-    if (n == -1)
+    if (stat(c, &st) == -1)
+    {
+        perror("文件或目录不存在或无法访问");
+        exit(1);
+    }
+    if (S_ISREG(st.st_mode))
+    {
+        return 1;
+    }
+    return 0;
+}
+void myls_l(const char *myd_name)
+{
+    struct stat st;
+    // printf("总计 %ld\n", st.st_blocks);
+    if (strcmp(myd_name, "..") != 0 && strncmp(myd_name, ".", 1))
+    {
+
+        if (stat(myd_name, &st) != -1)
+        {
+            if (S_ISDIR(st.st_mode))
+                printf("d");
+            else if (S_ISLNK(st.st_mode))
+                printf("l");
+            else
+                printf("-");
+            printf((st.st_mode & S_IRUSR) ? "r" : "-");
+            printf((st.st_mode & S_IWUSR) ? "w" : "-");
+            printf((st.st_mode & S_IXUSR) ? "x" : "-");
+            printf((st.st_mode & S_IRGRP) ? "r" : "-");
+            printf((st.st_mode & S_IWGRP) ? "w" : "-");
+            printf((st.st_mode & S_IXGRP) ? "x" : "-");
+            printf((st.st_mode & S_IROTH) ? "r" : "-");
+            printf((st.st_mode & S_IWOTH) ? "w" : "-");
+            printf((st.st_mode & S_IXOTH) ? "x" : "-");
+
+            printf(" %*lu", link_num_width, st.st_nlink);
+            printf(" %s", getpwuid(st.st_uid)->pw_name);
+            printf(" %s", getgrgid(st.st_gid)->gr_name);
+            printf(" %*lu", size_width, st.st_size);
+
+            char time_str[20];
+            struct tm *time = localtime(&st.st_mtime);
+            strftime(time_str, sizeof(time_str), "%m月 %d %H:%M", time);
+            printf(" %s ", time_str);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int c, n, m, a_flag = 0, l_flag = 0, t_flag = 0, r_flag = 0, i_flag = 0, s_flag = 0, flag = 0;
+    const char *path = (const char *)malloc(sizeof(int) * 2);
+    struct dirent **file;
+    if (argc == 1)
+    {
+        n = scandir(".", &file, NULL, filesort);
+    }
+    else
+    {
+        int j = 1, count = 0;
+        while (j <= argc - 1)
+        {
+            if (argv[j][0] != '-')
+            {
+                count++;
+                path = argv[j];
+                if (judge_file_or_directory(path))
+                {
+                    printf("%s", path);
+                }
+                if (count == 1)
+                {
+                    n = scandir(path, &file, NULL, filesort);
+                }
+                else
+                {
+                    m = scandir(path, &file, NULL, filesort);
+                }
+            }
+            j++;
+        }
+        if (!count)
+        {
+            n = scandir(".", &file, NULL, filesort);
+        }
+    }
+    if (n == -1 || m == -1)
     {
         perror("scandir");
         exit(EXIT_FAILURE);
     }
-    // printf("总计 %ld\n", st.st_blocks);
+    while ((c = getopt(argc, argv, "a::l::t::r::i::s::")) != -1)
+    {
+        switch (c)
+        {
+        case 'a':
+            a_flag = 1;
+            break;
+        case 'l':
+            l_flag = 1;
+            break;
+        case 't':
+            t_flag = 1;
+            break;
+        case 'r':
+            r_flag = 1;
+            break;
+        case 'i':
+            i_flag = 1;
+            break;
+        case 's':
+            s_flag = 1;
+            break;
+        }
+    }
     for (int i = 0; i < n; i++)
     {
-        if (strcmp(file[i]->d_name, "..") != 0 && strncmp(file[i]->d_name, ".", 1))
-        {
-
-            if (stat(file[i]->d_name, &st) != -1)
-            {
-                if (S_ISDIR(st.st_mode))
-                    printf("d");
-                else if (S_ISLNK(st.st_mode))
-                    printf("l");
-                else
-                    printf("-");
-                printf((st.st_mode & S_IRUSR) ? "r" : "-");
-                printf((st.st_mode & S_IWUSR) ? "w" : "-");
-                printf((st.st_mode & S_IXUSR) ? "x" : "-");
-                printf((st.st_mode & S_IRGRP) ? "r" : "-");
-                printf((st.st_mode & S_IWGRP) ? "w" : "-");
-                printf((st.st_mode & S_IXGRP) ? "x" : "-");
-                printf((st.st_mode & S_IROTH) ? "r" : "-");
-                printf((st.st_mode & S_IWOTH) ? "w" : "-");
-                printf((st.st_mode & S_IXOTH) ? "x" : "-");
-
-                printf(" %*lu", link_num_width, st.st_nlink);
-                printf(" %s", getpwuid(st.st_uid)->pw_name);
-                printf(" %s", getgrgid(st.st_gid)->gr_name);
-                printf(" %*lu", size_width, st.st_size);
-
-                char time_str[20];
-                struct tm *time = localtime(&st.st_mtime);
-                strftime(time_str, sizeof(time_str), "%m月 %d %H:%M", time);
-                printf(" %s", time_str);
-                printf(" %s\n", file[i]->d_name);
-            }
-            free(file[i]);
-        }
-    }
-    free(file);
-}
-void myls_t(struct dirent **file) {}
-void myls_r(struct dirent **file) {}
-void myls_i(struct dirent **file) {}
-int main(int argc, char *argv[])
-{
-    int c, a_flag = 0, l_flag = 0, t_flag = 0, r_flag = 0, i_flag = 0, s_flag = 0;
-    char s[10];
-    struct dirent **file;
-    if (argc == 1)
-    {
-        mylsbase(file, ".");
-    }
-    else if (argc == 2 && argv[1][0] != '-')
-    {
-        mylsbase(file, argv[1]);
-    }
-    else
-    {
-        while ((c = getopt(argc, argv, "a::l::t::r::i::s::")) != -1)
-        {
-            switch (c)
-            {
-            case 'a':
-                a_flag = 1;
-                break;
-            case 'l':
-                l_flag = 1;
-                break;
-            case 't':
-                t_flag = 1;
-                myls_t(file);
-                break;
-            case 'r':
-                r_flag = 1;
-                myls_r(file);
-                break;
-            case 'i':
-                i_flag = 1;
-                myls_i(file);
-                break;
-            case 's':
-                s_flag = 1;
-                myls_s(file);
-                break;
-            }
-        }
         if (l_flag)
         {
-            if (argc == 3 && a_flag == 0)
+            flag = 1;
+            if (a_flag == 1)
             {
-                myls_l(file, argv[2]);
+                myls_l(file[i]->d_name);
             }
-            else
+            else if (a_flag == 0)
             {
-                myls_l(file, ".");
+                if (strcmp(file[i]->d_name, "..") != 0 && strncmp(file[i]->d_name, ".", 1) != 0)
+                {
+                    myls_l(file[i]->d_name);
+                    printf("%s\n", file[i]->d_name);
+                }
             }
         }
         if (a_flag)
         {
-            if (argc == 3 && l_flag == 0)
-            {
-                myls_a(file, argv[2]);
-            }
-            else
-            {
-                myls_a(file, ".");
-            }
+            flag = 1;
+            printf("%s\n", file[i]->d_name);
         }
         if (t_flag)
         {
-            myls_t(file);
+            flag = 1;
+            // myls_t(file);
         }
-
         if (r_flag)
         {
-            myls_r(file);
+            flag = 1;
+            // myls_r(file);
         }
-
         if (i_flag)
         {
-            myls_i(file);
+            flag = 1;
+            // myls_i(file);
         }
-
         if (s_flag)
         {
-            myls_s(file);
+            flag = 1;
+            // myls_s(file);
         }
+        if (flag == 0)
+        {
+            if (strcmp(file[i]->d_name, "..") != 0 && strncmp(file[i]->d_name, ".", 1) != 0)
+            {
+                printf("%s\n", file[i]->d_name);
+            }
+        }
+        free(file[i]);
     }
+    free(file);
     return 0;
 }
